97504f2658a847918d18e40737ca2dfac63457e8
Jacob <50183874+jacoburantowka@users.noreply.github.com>
2023-11-30 13:56:43 +0000

    Add initial game logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <script setup>
+import { ref, computed } from 'vue'
 
+const player = ref('x')
+const board = ref([
+	['', '', ''],
+	['', '', ''],
+	['', '', ''],
+])
+
+const calculateWinner = (board) => {
+	const winningLines = [[0, 1, 2]]
+
+	for (let i = 0; i < winningLines.length; i++) {
+		const [a, b, c] = winningLines[i]
+		if (board[a] && board[a] === board[b] && board === board[c]) {
+			return board[a]
+		}
+		return null
+	}
+}
+
+const winner = computed(() => calculateWinner(board?.value.flat()))
+
+const makeMove = (x, y) => {
+	if (winner.value) return
+
+	if (board.value[x][y] !== '') return
+
+	board.value[x][y] = player.value
+
+	player.value = player.value === 'x' ? 'o' : 'x'
+}
+
+const resetGame = () => {
+	board.value = [
+		['', '', ''],
+		['', '', ''],
+		['', '', ''],
+	]
+	player.value = 'x'
+}
 </script>
 
 <template>
-  <div>
-    Henlo!
-  </div>
+	<div>Henlo!</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
